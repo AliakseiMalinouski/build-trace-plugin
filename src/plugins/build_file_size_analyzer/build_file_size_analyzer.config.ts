b69c6d54a5fbd6dd362d8f70039eaec619c826cc
AliakseiMalinouski <aleksymalinowski21@gmail.com>
2025-12-11 21:24:37 +0100

    fixed build file size options

--- src/plugins/build_file_size_analyzer/build_file_size_analyzer.config.ts
+++ src/plugins/build_file_size_analyzer/build_file_size_analyzer.config.ts
@@ -1,7 +1,5 @@
 import { BuildFileSizeConfigType } from "./build_file_size.analyzer.types";
 
 export const BuildFileSizeAnalyzerConfig: BuildFileSizeConfigType = {
-    outputDir: 'build',
-    outputFile: 'main.js',
     active: false,
 };
